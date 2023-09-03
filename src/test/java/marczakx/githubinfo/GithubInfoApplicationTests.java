65d0ec7f9c34285992a70644cbb9d06edd80fe8d
tom <marczakx@gmail.com>
2023-09-03 17:36:55 +0200

    Inital spring boot

---
+++ src/test/java/marczakx/githubinfo/GithubInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package marczakx.githubinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
